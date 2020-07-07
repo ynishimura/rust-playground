2abfd1563761c88ee1b675bffc7236a23812feef
ynishimura <ynishimura0922@gmail.com>
2020-07-07 18:54:33 +0900

    ベクター使い方

--- tutorial/hoge/src/main.rs
+++ tutorial/hoge/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
+    let mut v: Vec<i32> = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+    v.push(9);
+    v.push(10);
+    // let first = &v[0];
+
+    // v.push(9);
+    for i in &mut v {
+        *i += 50;
+        println!("{}",i);
+    }
+    // println!("{}", first);
     println!("Hello, world!");
 }
