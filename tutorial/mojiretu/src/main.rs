81ee28b24828d2e87ca118787c350b4356ce70dd
ynishimura <ynishimura0922@gmail.com>
2020-07-07 21:53:04 +0900

    文字列の扱い

--- tutorial/mojiretu/src/main.rs
+++ tutorial/mojiretu/src/main.rs
@@ -8,5 +8,24 @@ fn main() {
     // let s =String::from("initial cotents");
 
     // let s = "initia; contents".to_string();
+    let mut s1 = String::from("foo");
+    let s2 = "boo";
+    s1.push_str(s2);
+    println!("s1 is {}", s1);
+    println!("s2 is {}", &s2);
+    // 問題なく動く。push_strは所有権を得なくてもいいメソッド
+
     println!("Hello, world! {}", s);
+
+    let sum = format!("{}-{}-{}", s, s1, s2);
+    // format!は引数の所有権を奪わない
+    println!("Hello, world! {}", sum);
+
+    let hello = "3afffda";
+    let s = &hello[0..4]; // 3aff
+    println!("{}", s);
+
+    for c in "プログラミング言語Rust".chars() {
+        println!("{}", c);
+    }
 }
