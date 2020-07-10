d5c06b99cbf24697c2891090acbf07b29f22f55b
ynishimura <ynishimura0922@gmail.com>
2020-07-10 21:47:21 +0900

    わざとパニックを発生（unwrap）

--- panic-test/src/main.rs
+++ panic-test/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
+// use std::io::ErrorKind;
 
 fn main() {
     println!("Hello, world!");
@@ -8,14 +8,15 @@ fn main() {
     // let v = vec![1, 2, 3];
     // v[99];
 
-    let f = File::open("hello.txt");
+    // hello.txtなしで走らせるとパニック
+    let f = File::open("hello.txt").unwrap();
 
-    let f = match f {
-        Ok(file) => file,
-        Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
-            Ok(fc) => fc,
-            Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
-        },
-        Err(error) => panic!("There was a problem opening the file: {:?}", error),
-    };
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
+    //         Ok(fc) => fc,
+    //         Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+    //     },
+    //     Err(error) => panic!("There was a problem opening the file: {:?}", error),
+    // };
 }
