ec16d0d862ab1c70c46249a899e59ed7e8a3c1b7
ynishimura <ynishimura0922@gmail.com>
2020-07-12 16:20:11 +0900

    ライフタイム付与

--- life/src/main.rs
+++ life/src/main.rs
@@ -9,11 +9,19 @@ fn main() {
 
     // println!("r: {}", r);
 
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    // let string1 = String::from("abcd");
+    // let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+    // let result = longest(string1.as_str(), string2);
+    // println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
