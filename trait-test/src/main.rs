720cb28b2eb42aa42fdb2ea0e7eaf5b90059fef2
ynishimura <ynishimura0922@gmail.com>
2020-07-12 14:02:04 +0900

    デフォルト実装

--- trait-test/src/main.rs
+++ trait-test/src/main.rs
@@ -1,7 +1,9 @@
 pub trait Summary {
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        // defalt impl
-        String::from("(READ more...)")
+        // {}さんからもっと読む
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
 
@@ -12,8 +14,11 @@ pub struct NewsArticle {
     pub content: String,
 }
 impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.headline)
+    }
     fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
+        format!("{}", self.summarize_author())
     }
 }
 
@@ -24,8 +29,8 @@ pub struct Tweet {
     pub retweet: bool,
 }
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
 
@@ -36,5 +41,19 @@ fn main() {
         reply: false,
         retweet: false,
     };
+    let article = NewsArticle {
+        // ペンギンチームがスタンレーカップチャンピオンシップを勝ち取る！
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        // ピッツバーグ、ペンシルベニア州、アメリカ
+        location: String::from("Pittsburgh, PA, USA"),
+        // アイスバーグ
+        author: String::from("Iceburgh"),
+        // ピッツバーグ・ペンギンが再度NHL(National Hockey League)で最強のホッケーチームになった
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best
+        hockey team in the NHL.",
+        ),
+    };
+    println!("New article available!　=> {}", article.summarize());
     print!("1 new tewwt: {}", tweet.summarize());
 }
