dd8372592a2ff9f1730c732e69bdfc64876abe6a
ynishimura <ynishimura0922@gmail.com>
2020-07-12 17:06:01 +0900

    構造体定義のライフタイム

--- life/src/main.rs
+++ life/src/main.rs
@@ -1,3 +1,15 @@
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     println!("Hello, world!");
     // let r;
@@ -21,13 +33,13 @@ fn main() {
         let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
     }
-    println!("The longest string is {}", result);
-}
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+    println!("The longest string is {}", result);
+
+    // 構造体定義のライフタイム注釈
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'"); // '.'が見つかりませんでした
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
