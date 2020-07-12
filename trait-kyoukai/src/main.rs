d9438d433ce30d6d7675d31182e4ecf4b2c35991
ynishimura <ynishimura0922@gmail.com>
2020-07-12 15:02:09 +0900

    トレイト境界

--- trait-kyoukai/src/main.rs
+++ trait-kyoukai/src/main.rs
@@ -12,6 +12,32 @@ fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     largest
 }
 
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+// Pair<T>は、
+// 内部の型Tが比較を可能にする
+// PartialOrdトレイトと出力を可能にするDisplayトレイトを実装している時のみ、
+// cmp_displayメソッドを実装します。
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -22,4 +48,14 @@ fn main() {
 
     let result = largest(&char_list);
     println!("The largest char is {}", result);
+
+    // トレイト境界をつかった実装を実行
+    let p1 = Pair::new(10, 20);
+    let p2 = Pair::new(-9.99, -20.0);
+    p1.cmp_display();
+    p2.cmp_display();
+
+    // 比較できない型をいれてみる(Displayトレイトを持たない)
+    let p3 = Pair::new(vec![1, 2, 3], vec![4, 5, 6]);
+    p3.cmp_display();
 }
