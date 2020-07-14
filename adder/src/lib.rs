cb549fc0479ffbbedd7ce79eafad0dc0a354148e
ynishimura <ynishimura0922@gmail.com>
2020-07-14 22:50:52 +0900

    should_panicでパニックを確認する

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -43,6 +43,11 @@ mod tests {
             result
         );
     }
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
 }
 
 #[derive(Debug)]
@@ -60,6 +65,21 @@ impl Rectangle {
 
 pub fn greeting(name: &str) -> String {
     // こんにちは、{}さん！
-    // format!("Hello {}!", name)
-    String::from("Hello!")
+    format!("Hello {}!", name)
+    // String::from("Hello!")
+}
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            //予想値は1から100の間でなければなりません
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
 }
