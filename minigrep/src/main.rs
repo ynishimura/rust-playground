d193baed27dc696d16bdd542cf0f1bc66d33c3f8
ynishimura <ynishimura0922@gmail.com>
2020-07-16 23:09:25 +0900

    ファイル読み込み

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,5 +9,15 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
-    println!("{:?}", args);
+    // ファイルが見つかりませんでした
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        // ファイルの読み込み中に問題がありました
+        .expect("something went wrong reading the file");
+
+    // テキストは\n{}です
+    println!("With text:\n{}", contents);
+    // println!("{:?}", args);
 }
