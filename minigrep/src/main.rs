78e2611c978f8647dc26e7c78ca513fd0b26cdec
ynishimura <ynishimura0922@gmail.com>
2020-07-16 23:28:49 +0900

    ファイル読み込みロジックを関数化

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,13 +4,12 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
     // ファイルが見つかりませんでした
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
@@ -20,4 +19,16 @@ fn main() {
     // テキストは\n{}です
     println!("With text:\n{}", contents);
     // println!("{:?}", args);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
