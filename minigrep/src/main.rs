da40d162f94ca5e6e3e70a80b2a9dcad268a6e1b
ynishimura <ynishimura0922@gmail.com>
2020-07-17 23:44:46 +0900

    エラーは呼び出し元で処理する
    ココではmain関数で処理

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
@@ -33,15 +34,14 @@ impl Config {
     }
 }
 
-fn run(config: Config) {
+fn run(config: Config) -> Result<(), Box<Error>> {
     // ファイルが見つかりませんでした
-    let mut f = File::open(config.filename).expect("file not found");
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        // ファイルの読み込み中に問題がありました
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     // テキストは\n{}です
     println!("With text:\n{}", contents);
+    Ok(())
 }
