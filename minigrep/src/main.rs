3b500c2418e6333fa0f10569c0a4e8a22fc2ef90
ynishimura <ynishimura0922@gmail.com>
2020-07-17 23:35:09 +0900

    エラー処理
    ユーザフレンドリーな表示にする

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
