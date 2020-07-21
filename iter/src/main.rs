6060b7777b3cf1ff12115d5711214aa94c3fbb48
ynishimura <ynishimura0922@gmail.com>
2020-07-21 23:19:38 +0900

    キャッシュ用の構造体追加

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -7,12 +7,13 @@ fn generate_workout(intensity: u32, random_number: u32) {
         thread::sleep(Duration::from_secs(2));
         num
     };
+
     if intensity < 25 {
         println!("Today, do {} pushups!", expensive_closure(intensity));
         println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
-            println!("Take a break today! Remember to stay hydated!");
+            println!("Take a break today! Remember to stay hydrated!");
         } else {
             println!("Today, run for {} minutes!", expensive_closure(intensity));
         }
@@ -24,4 +25,34 @@ fn main() {
     let random_number = 7;
     generate_workout(user_specified_value, random_number);
     println!("Hello, world!");
+}
+
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculaton: T,
+    value: Option<u32>,
+}
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
 }
