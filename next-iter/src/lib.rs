47c9546df2c4b6a4fd0466f38b3c1d356aee4f41
ynishimura <ynishimura0922@gmail.com>
2020-07-23 00:16:24 +0900

    イテレータとfilter

--- next-iter/src/lib.rs
+++ next-iter/src/lib.rs
@@ -12,3 +12,49 @@ fn iterator_demonstration() {
     let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
     assert_eq!(v2, vec![2, 3, 4]);
 }
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    // into_iterを呼び出してベクタの所有権を奪うイテレータを作成
+    // collectを呼び出すと、 関数により返ってきたベクタに適合させたイテレータから返ってきた値が集まる
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+#[test]
+fn filters_by_size() {
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot")
+            },
+        ]
+    );
+}
