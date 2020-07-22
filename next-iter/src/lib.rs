093e26134b1344fd4cb9c671a30f6b34cd284830
ynishimura <ynishimura0922@gmail.com>
2020-07-22 23:29:01 +0900

    collectを使う

--- next-iter/src/lib.rs
+++ next-iter/src/lib.rs
@@ -5,3 +5,10 @@ fn iterator_sum() {
     let total: i32 = v1_iter.sum();
     assert_eq!(total, 6);
 }
+
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+    let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
+}
