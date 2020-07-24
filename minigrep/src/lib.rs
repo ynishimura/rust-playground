356f9dc85a0f66426ee608e6cc903c21493484d8
ynishimura <ynishimura0922@gmail.com>
2020-07-24 13:58:49 +0900

    serach 関数をリファクタリング

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -50,13 +50,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    // filterでマッチした行を取得
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
