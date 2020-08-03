9dea38d0aecd02ec8368b0efeef2c533733d740f
ynishimura <ynishimura0922@gmail.com>
2020-08-03 23:40:27 +0900

    Derefトレイトを実装して型を参照にように扱う

--- box-test/src/main.rs
+++ box-test/src/main.rs
@@ -24,9 +24,21 @@ fn main() {
     // assert_eq!(5, y);
 }
 
+use std::ops::Deref;
+
 struct MyBox<T>(T);
 impl<T> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
         MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    // Derefトレイトが使用する関連型を定義しています
+    type Target = T;
+    // selfを借用し、 内部のデータへの参照を返すderefという1つのメソッドを実装する
+    // derefは*演算子でアクセスしたい値への参照を返します。
+    fn deref(&self) -> &T {
+        &self.0
     }
 }
