e164d5808fa048aff3820495c024e043d56d4730
ynishimura <ynishimura0922@gmail.com>
2020-08-03 23:09:25 +0900

    Boxを使ってエラーを解消

--- box-test/src/main.rs
+++ box-test/src/main.rs
@@ -1,12 +1,14 @@
+// 無限のサイズと判定されてコンパイルエラー
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
-    let list = Cons(1, Cons(2, Cons(3, Nil)));
-}
-
-// 無限のサイズと判定されてコンパイルエラー
-enum List {
-    Cons(i32, List),
-    Nil,
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
