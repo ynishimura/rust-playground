d0e2cac6c80f0bdfc948ca9f51de81090e067300
ynishimura <ynishimura0922@gmail.com>
2020-08-06 00:07:11 +0900

    dropトレイトのコード追加

--- drop-test/src/main.rs
+++ drop-test/src/main.rs
@@ -1,3 +1,23 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    // dはcより先にドロップされるため下記出力となる
+    // Dropping CustomSmartPointer with data `other stuff`!
+    // Dropping CustomSmartPointer with data `my stuff`!
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
 }
