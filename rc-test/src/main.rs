abb3b3a87f733d3ab6639be219468d93ce067bef
ynishimura <ynishimura0922@gmail.com>
2020-08-10 20:57:19 +0900

    Rcを使うとエラーが解消される

--- rc-test/src/main.rs
+++ rc-test/src/main.rs
@@ -1,16 +1,17 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
+use std::rc::Rc;
 use List::{Cons, Nil};
 
 fn main() {
     // 5と10を含むリストaを作ります。
-    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
 
     // 3で始まるbと4で始まるcをつくる
     // 5と10を含む最初のaリストを含めるとエラー。
-    let b = Cons(3, Box::new(a));
-    let c = Cons(4, Box::new(a));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
