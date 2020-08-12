40389d121b432a2141253e6106743dd25d12493d
ynishimura <ynishimura0922@gmail.com>
2020-08-12 22:14:09 +0900

    借用チェッカーが許可してくれないMockMessangerを実装しようとする

--- refcell-tet/src/lib.rs
+++ refcell-tet/src/lib.rs
@@ -39,3 +39,30 @@ where
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockMessenger {
+        send_messages: Vec<String>,
+    }
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                send_messages: vec![],
+            }
+        }
+    }
+    impl Messnger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.send_messages.push(String::from(message));
+        }
+    }
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+        limit_tracker.set_value(80);
+        assert_eq!(mock_messenger.sent_messages.len(), 1);
+}
