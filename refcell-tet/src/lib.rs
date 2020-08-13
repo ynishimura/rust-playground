41096a9e48d7a125cc5caccc9dca0c3504626919
ynishimura <ynishimura0922@gmail.com>
2020-08-13 23:36:59 +0900

    外側の値は不変と考えられる一方でRefCell<T>で内部の値を可変化する

--- refcell-tet/src/lib.rs
+++ refcell-tet/src/lib.rs
@@ -40,23 +40,25 @@ where
     }
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::cell::RefCell;
 
     struct MockMessenger {
-        send_messages: Vec<String>,
+        sent_messages: RefCell<Vec<String>>,
     }
+
     impl MockMessenger {
         fn new() -> MockMessenger {
-            MockMessenger {
-                send_messages: vec![],
-            }
+            MockMessenger { sent_messages: RefCell::new(vec![]) }
         }
     }
-    impl Messnger for MockMessenger {
+
+    impl Messenger for MockMessenger {
         fn send(&self, message: &str) {
-            self.send_messages.push(String::from(message));
+            self.sent_messages.borrow_mut().push(String::from(message));
         }
     }
     #[test]
@@ -64,5 +66,5 @@ mod tests {
         let mock_messenger = MockMessenger::new();
         let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
         limit_tracker.set_value(80);
-        assert_eq!(mock_messenger.sent_messages.len(), 1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
 }
